076203b0db678491fbfdf72279924c0327985ea7
Grover <sgrover@cs.princeton.edu>
2016-01-27 15:59:05 -0500

    separate function for all country

--- topsites.py
+++ topsites.py
@@ -3,19 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 from math import ceil
+from collections import defaultdict
+import csv
 
 BASE_URL='http://www.alexa.com/topsites/countries;%d/%s'
 
-if __name__ == '__main__':
+def get_top_country(country_code, number):
 
-    if len(sys.argv) != 3:
-        sys.stderr.write('Usage: COUNTRY-CODE TOP-N\n')
-        sys.exit(1)
-
-    country_code = sys.argv[1].upper()
-    number = int(sys.argv[2])
-    delimiter = ' '
-
+    data = defaultdict(list)
     page_numbers = int(ceil(number/25.0))
 
     for page_num in range(0, page_numbers):
@@ -25,6 +20,30 @@ if __name__ == '__main__':
         bullets = soup.find_all('li', {'class':'site-listing'})
 
         for bullet in bullets:
-            rank = bullet.div.contents[0]
-            site = bullet.p.a.contents[0]
-            print('%s%s%s' % (rank, delimiter, site))
+            data['rank'].append(bullet.div.contents[0])
+            data['site'].append(bullet.p.a.contents[0])
+
+    filename = country_code + "_" + number + ".csv"
+    with open("output/" + filename, 'w') as f:
+        w = csv.DictWriter(f, data.keys())
+        w.writeheader()
+        w.writerow(data)
+
+    return
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: python topsites.py {COUNTRY-CODE, ALL} TOP-N\n')
+        sys.exit(1)
+
+    country_code = sys.argv[1].upper()
+    number = int(sys.argv[2])
+
+    if country_code == 'ALL':
+        pass
+    else:
+        get_top_country(country_code, number)
+
+
+
